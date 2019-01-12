5e7f21f792978e572c0a3ca9a4066d1c3d6e8946
Marie-Eve Dubé <mariedube2@gmail.com>
2019-01-12 17:21:27 -0500

    init of QT project with opengl

---
+++ src/openglwindow.h
@@ -0,0 +1,36 @@
+#include <QtGui/QWindow>
+#include <QtGui/QOpenGLFunctions>
+
+class QPainter;
+class QOpenGLContext;
+class QOpenGLPaintDevice;
+
+class OpenGLWindow : public QWindow, protected QOpenGLFunctions
+{
+  Q_OBJECT
+public:
+  explicit OpenGLWindow(QWindow *parent = 0);
+  ~OpenGLWindow();
+
+  virtual void render(QPainter *painter);
+  virtual void render();
+
+  virtual void initialize();
+
+  void setAnimating(bool animating);
+
+public slots:
+  void renderLater();
+  void renderNow();
+
+protected:
+  bool event(QEvent *event) override;
+
+  void exposeEvent(QExposeEvent *event) override;
+
+private:
+  bool m_animating;
+
+  QOpenGLContext *m_context;
+  QOpenGLPaintDevice *m_device;
+};
